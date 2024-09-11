eec056d40c4a909ab8035f769e1604ff9e255847
Ryusei Yamaguchi <mandel59@gmail.com>
2024-09-11 09:48:14 +0900

    Fix to exclude kFanqie from unihan_fts

--- api/v1/_lib/query-expressions.ts
+++ api/v1/_lib/query-expressions.ts
@@ -98,7 +98,7 @@ export const queryExpressions = [
           unihan.value glob printf('*%s*', @ucs)
           OR (unihan.value glob printf('*U+%04X*', unicode(@ucs))
             AND NOT unihan.value glob printf('*U+%04X[0-9A-F]*', unicode(@ucs))))
-          AND unihan.property NOT IN ('kJapanese', 'kSMSZD2003Readings')
+          AND unihan.property NOT IN ('kJapanese', 'kSMSZD2003Readings', 'kFanqie')
         ORDER BY UCS
         LIMIT 100))`,
   ],
